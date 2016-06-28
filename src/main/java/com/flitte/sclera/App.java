ca9fe76c65beb8386d8358652b3a7c2f68d25057
flitte <flitte@users.noreply.github.com>
2016-06-28 17:22:01 +0100

    Updated with latest changes.

--- src/main/java/com/flitte/sclera/App.java
+++ src/main/java/com/flitte/sclera/App.java
@@ -2,9 +2,12 @@ package com.flitte.sclera;
 
 import gaffer.store.schema.Schema;
 import org.graphstream.graph.Graph;
-import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.graph.implementations.MultiGraph;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Map;
 
 import static java.lang.Boolean.parseBoolean;
 
@@ -14,43 +17,52 @@ import static java.lang.Boolean.parseBoolean;
  */
 public class App {
 
-    public static void main(final String args[]) {
+    public static void main(final String args[]) throws FileNotFoundException {
 
-        final InputStream dataSchema = App.class.getResourceAsStream("/dataSchema.json");
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+
+        final InputStream dataSchema = new FileInputStream(args[0]);
         final InputStream dataTypes = App.class.getResourceAsStream("/dataTypes.json");
         final InputStream storeTypes = App.class.getResourceAsStream("/storeTypes.json");
 
         final Schema schema = Schema.fromJson(dataSchema);
 
-        System.out.println(schema.toString());
+        final Graph graph = new MultiGraph("Schema");
 
-        System.out.println(schema.getEdges()
-                                 .toString());
-
-        System.out.println(schema.getEntities()
-                                 .toString());
-
-        final Graph graph = new SingleGraph("Test");
+        graph.addAttribute("ui.quality");
+        graph.addAttribute("ui.antialias");
+        graph.addAttribute("ui.stylesheet", "url('file:///home/tom/projects/flitte/sclera/src/main/resources/gs.css')");
 
         schema.getEntities()
               .forEach((k, v) -> {
                   // Plot vertices (if not already added)...
                   if (graph.getNode(v.getVertex()) == null) {
-                      System.out.println("Added node: " + v.getVertex());
-                      graph.addNode(v.getVertex());
+                      graph.addNode(v.getVertex())
+                           .setAttribute("ui.class", "vertex");
                   }
 
                   // Plot entities...
-                  graph.addNode(k);
-                  System.out.println("Added node: " + k);
+                  graph.addNode(k)
+                       .setAttribute("ui.class", "entity");
+
+                  // Plot entity properties...
+                  graph.addNode(getPropertiesNodeText(v.getPropertyMap()))
+                       .setAttribute("ui.class", "properties");
 
                   // Connect nodes to vertices...
-                  graph.addEdge(k, v.getVertex(), k, false);
+                  graph.addEdge(k, v.getVertex(), k, false)
+                       .setAttribute("ui.class", "entitytovertex");
+
+                  // Connect entity nodes to entity property nodes...
+                  graph.addEdge(k + "_properties", k, getPropertiesNodeText(v.getPropertyMap()))
+                       .setAttribute("ui.class", "properties");
               });
 
+        // Add edges between vertices...
         schema.getEdges()
               .forEach((k, v) -> {
-                  graph.addEdge(k, v.getSource(), v.getDestination(), Boolean.parseBoolean(v.getDirected()));
+                  graph.addEdge(k, v.getSource(), v.getDestination(), parseBoolean(v.getDirected()))
+                       .setAttribute("ui.class", "vertextovertex");
               });
 
         graph.getNodeSet()
@@ -59,14 +71,20 @@ public class App {
         graph.getEdgeSet()
              .forEach(edge -> edge.setAttribute("ui.label", edge.getId()));
 
-//        graph.addNode("A");
-//        graph.addNode("B");
-//        graph.addNode("C");
-//        graph.addEdge("AB", "A", "B");
-//        graph.addEdge("BC", "B", "C");
-//        graph.addEdge("CA", "C", "A");
-
         graph.display();
+    }
+
+    private static String getPropertiesNodeText(final Map<String, String> map) {
+
+        final StringBuilder sb = new StringBuilder();
+
+        map.forEach((k, v) -> sb.append(k)
+                                .append(": ")
+                                .append(v)
+                                .append(", "));
+
+        return sb.toString();
+//                 .substring(0, sb.length() - 2);
     }
 
 }
